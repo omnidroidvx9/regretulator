2acd4629f88c9ab301e9939d69c1a5c46c46588c
Jason Baker <jason.edward.baker@gmail.com>
2025-01-01 06:36:30 -0500

    Updated expected result for automated test.

--- tests/cypress/e2e/regretulator.cy.js
+++ tests/cypress/e2e/regretulator.cy.js
@@ -13,7 +13,7 @@ describe('example to-do app', () => {
   it('calculates regret', () => {
     const year = '2004'
     const amount = 8000
-    const expectedResult = '$50,531.10'
+    const expectedResult = '$63,173.98'
 
     page.selectYear(year)  
     page.enterAmount(`${amount}`)
